c542998d32038b36798431b07da055c393db1531
REDxEYE <med45c@gmail.com>
2020-07-06 20:58:10 +0300

    fixed stupid blender bug with selected objects

--- mesh_operators.py
+++ mesh_operators.py
@@ -10,13 +10,13 @@ class SHAPE_KEYS_OT_TransferShapes(bpy.types.Operator):
     bl_label = "Transfer shapes"
     bl_options = {'REGISTER', 'UNDO'}
 
-    def pool(self, context):
-        return len(context.selected_objects) == 2
-
     def execute(self, context):
         objs = context.selected_objects
+        target = context.active_object
 
-        source, target = objs
+        objs.remove(target)
+        source = objs[0]
+        print(source, target)
 
         src_vertices = np.zeros((len(source.data.vertices) * 3,), dtype=np.float32)
         source.data.vertices.foreach_get('co', src_vertices)
@@ -25,6 +25,7 @@ class SHAPE_KEYS_OT_TransferShapes(bpy.types.Operator):
         target.data.vertices.foreach_get('co', dst_vertices)
 
         for src_shape_key in source.data.shape_keys.key_blocks[1:]:
+            print(f"Transferring {src_shape_key.name}")
             src_shape_vertices = np.zeros((len(source.data.vertices) * 3,), dtype=np.float32)
             src_shape_key.data.foreach_get('co', src_shape_vertices)
 

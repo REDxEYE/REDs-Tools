0aab2c915c082ade4b29aca2214a2f1d7ca8050d
LoveRenamon <love_renamon@hotmail.com>
2024-12-04 03:07:41 -0300

    copy_pose.py ensure behaviours 	now works only with armatures, so theres no exceptions 	add support to multiple armature be parsed at once

--- copy_pose.py
+++ copy_pose.py
@@ -15,26 +15,29 @@ class VALVE_OT_CopyPose(bpy.types.Operator):
     @classmethod
     def poll(cls, context):
         # Check if both armatures are selected
-        if len(context.selected_objects) != 2:
+        armatures = [ob for ob in context.selected_objects if ob.type == 'ARMATURE']
+        if len(armatures) < 2:
+            cls.poll_message_set("You need select two Armatures")
             return False
-        armatures = [obj for obj in context.selected_objects if obj.type == 'ARMATURE']
-        if len(armatures) != 2:
+        if not context.active_object.type == 'ARMATURE':
+            cls.poll_message_set("Active Object must be Armature")
             return False
         return True
 
     def execute(self, context):
-        ref_ob = bpy.context.object
-        other_ob = [ob for ob in bpy.data.objects if ob.select_get()]
+        ref_ob = context.object
+        other_ob = [ob for ob in bpy.data.objects if ob.select_get() and ob.type == 'ARMATURE']
         other_ob.remove(ref_ob)
-        other_ob = other_ob[0]
+        #ob = other_ob[0] # target only one armature
 
-        bpy.context.view_layer.objects.active = other_ob
+        for ob in other_ob:
+            context.view_layer.objects.active = ob
 
-        bpy.ops.object.mode_set(mode='OBJECT')
-        for bone in ref_ob.pose.bones:
-            if bone.name in other_ob.pose.bones:
-                bpy.ops.object.mode_set(mode='POSE')
-                other_ob.pose.bones[bone.name].matrix = bone.matrix
-                bpy.ops.object.mode_set(mode='OBJECT')
+            bpy.ops.object.mode_set(mode='POSE')
+            for bone in ref_ob.pose.bones:
+                if bone.name in ob.pose.bones:
+                    bpy.ops.object.mode_set(mode='POSE')
+                    ob.pose.bones[bone.name].matrix = bone.matrix
+                    bpy.ops.object.mode_set(mode='OBJECT')
 
         return {'FINISHED'}
